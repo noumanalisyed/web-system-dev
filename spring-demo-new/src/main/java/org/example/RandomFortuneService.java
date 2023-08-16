24c9089c9dbb74511efbdf4e007c2d33c747ba64
noumanalisyed <nouman.ali.syed@gmail.com>
2023-08-16 13:23:29 +0500

    Random Fortune Services added

---
+++ spring-demo-new/src/main/java/org/example/RandomFortuneService.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Random;
+
+public class RandomFortuneService implements FortuneService{
+
+    String strMessages [] = {"A","B","C","D","E","F"};
+    private int getRandomIndex(){
+        return new Random().nextInt(6);
+    }
+    @Override
+    public String getDailyFortune() {
+        return "RandomFortuneService -- "+strMessages[getRandomIndex()];
+    }
+}
